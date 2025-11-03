fdd4c5de5efe530939f6fad9fdd1ae4a5e295f8c
omagemz <omagemz@gmail.com>
2025-11-03 17:55:33 +0400

    Import index.css in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
